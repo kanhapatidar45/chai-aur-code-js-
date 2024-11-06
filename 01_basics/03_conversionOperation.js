7a4bd8bf80eddb271745a16a06806ec94a89d42f
Kanha patidar <124128569+kanhapatidar45@users.noreply.github.com>
2024-11-06 09:59:37 +0000

    datatype-summary

--- 01_basics/03_conversionOperation.js
+++ 01_basics/03_conversionOperation.js
@@ -1,63 +1,111 @@
-let score = 33
-console.log(typeof score);
-console.log(typeof (score))
+// let score = 33
+// console.log(typeof score);
+// console.log(typeof (score))
 
-let marks ="34aa"
+// let marks ="34aa"
 
-console.log(typeof marks);
-console.log(typeof (marks))
+// console.log(typeof marks);
+// console.log(typeof (marks))
 
-//conversion from string to number
-let valueInNumber=Number(marks);
-console.log(typeof valueInNumber);
-console.log(valueInNumber)
+// //conversion from string to number
+// let valueInNumber=Number(marks);
+// console.log(typeof valueInNumber);
+// console.log(valueInNumber)
 
-let target=null;
-let valTarget=Number(target)
-console.log(valTarget)
+// let target=null;
+// let valTarget=Number(target)
+// console.log(valTarget)
 
-let state= undefined
-let valState= Number(state)
-console.log(valState)
+// let state= undefined
+// let valState= Number(state)
+// console.log(valState)
 
-let boolean= true;
-let valBoolean=Number(boolean)
-console.log( valBoolean)
+// let boolean= true;
+// let valBoolean=Number(boolean)
+// console.log( valBoolean)
 
-/*
-conversion from any datatype to number
-//"33"=>33
-//"33abc"=>NAN
-//true=>1, false=>0
-//null=>0
-//undefined=>NAN
- */
-
-
-//conversion fron datatype to boolean
-let number=1;
-let valNumber=Boolean(number);
-console.log(valNumber)
-
-let num=0;
-let valNum=Boolean(num);
-console.log(valNum)
-
-let nam="";
-let valnam=Boolean(nam);
-console.log(valnam)
-
-let newnam="hardik";
-let valnewnam=Boolean(newnam);
-console.log(valnewnam)
+// /*
+// conversion from any datatype to number
+// //"33"=>33
+// //"33abc"=>NAN
+// //true=>1, false=>0
+// //null=>0
+// //undefined=>NAN
+//  */
 
 
-//1=>true, 0=>false
-//""=>false
-//"hitesh"=>true
+// //conversion fron datatype to boolean
+// let number=1;
+// let valNumber=Boolean(number);
+// console.log(valNumber)
 
-//conversion to datatype to string
-let someNum=33
-let valSomeNum=String(someNum);
-console.log(valSomeNum)
-console.log(typeof valSomeNum)
+// let num=0;
+// let valNum=Boolean(num);
+// console.log(valNum)
+
+// let nam="";
+// let valnam=Boolean(nam);
+// console.log(valnam)
+
+// let newnam="hardik";
+// let valnewnam=Boolean(newnam);
+// console.log(valnewnam)
+
+
+// //1=>true, 0=>false
+// //""=>false
+// //"hitesh"=>true
+
+// //conversion to datatype to string
+// let someNum=33
+// let valSomeNum=String(someNum);
+// console.log(valSomeNum)
+// console.log(typeof valSomeNum)
+
+
+
+
+/************operations******** */
+
+// let value=3
+// let negValue=-value
+// console.log(negValue)
+
+// console.log(2 +2)
+// console.log(2 -2)
+// console.log(2 *2)
+// console.log(2 **3)
+// console.log(12 /3)
+// console.log(16%3)
+
+// let str1="hello"
+// let str2=" kanha"
+// let str3= str1+str2
+// console.log(str3)
+
+// console.log(1+"2")
+// console.log(1+2+"2")
+// console.log("1"+2)
+// console.log("1"+2+2)
+// console.log((1+2)*3)
+
+
+//comparison operator <,>,<=,>=,==,!=
+console.log(2>1)
+
+// console.log("2">1)//ye galat tarike h dono datatype sAME HONE chaiye comp operator use krte time
+
+// console.log(null>0)
+// console.log(null==0)
+// console.log(null>=0)
+
+// console.log(undefined>0)
+// console.log(undefined==0)
+// console.log(undefined>=0)
+
+
+//=== (check strictly value + datatype)
+console.log("2"==2)
+console.log("2"===2);
+
+
