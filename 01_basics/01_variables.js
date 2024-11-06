ba63dcb7b1e8b0722e301b48f6f26136bd8c3260
Kanha patidar <124128569+kanhapatidar45@users.noreply.github.com>
2024-11-06 06:41:09 +0000

    datatypes

---
+++ 01_basics/01_variables.js
@@ -0,0 +1,18 @@
+const accountId=1333;
+
+let accountEmail="kanha@gmail.com"
+
+var accountpassword="1234"
+accouncity="indore"
+let accountstate
+
+//accountId=44//not allowed
+accountEmail="hc@gmail.com"
+accountpassword="223"
+accouncity="bhopal"
+
+//let ka use sirf block ke ander hi kr sakte h var ka khi bhi isliye dont use var
+console.log(accountId);
+console.table([accountId,accountEmail,accountpassword,accouncity,accountstate]);
+
+
---
+++ 01_basics/02_datatype.js
@@ -0,0 +1,28 @@
+"use strict" // treat all js as newer version
+
+//alert (3 + 3) //we are using nodejs not browser
+
+
+//data types->
+1//string=>""
+let name="kanha"
+2//number=> range 2 to powr 53 agar isse bada h to bigint ka use krege
+let age=18
+3//boolean=> true/false
+let isLoggedIn=false
+4//null=>standard value
+let target=null
+
+5//undefined
+let state
+6//symbol =>unique
+
+
+
+
+//object
+
+console.log(typeof age)
+console.log(typeof null)
+console.log(typeof undefined)
+
