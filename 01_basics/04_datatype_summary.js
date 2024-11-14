15a1009dbd369e684d7d9263f8f34edd1aa50572
Kanha patidar <124128569+kanhapatidar45@users.noreply.github.com>
2024-11-14 09:25:47 +0000

    datatypesummary update

---
+++ 01_basics/04_datatype_summary.js
@@ -0,0 +1,49 @@
+//datatype are 2 type primitive and non primitive
+//primitive datatype are call by value type
+//7 types -> string, number,voolean, null,undefined,symbol, bigint
+
+// non primitve or reference type
+//3 types-> object array, function
+//js is dynamical language jaise 
+// const store=1
+//const store=false
+
+//symbol datatype
+// const id=Symbol('123')
+// const newId=Symbol('123')
+// console.log(id===newId)
+// // // symbol ka kaam hi yhi h ki dikh same rhi h but dono alg alg id h
+
+// // //bigint
+// // const bigNumber=2333434344n
+
+// //array
+// const heroes=["salman", "shahruk", "akshay"]
+// //object
+// let myObj={
+//     name:"hitesh",
+//     age:22,
+// }
+// //function
+// const myFunction=function(){
+//     console.log("hello world");
+
+// }
+
+//stack(primitve) and heap(non-primitive)
+let Myname="kanha"
+let anotherName=Myname
+anotherName="hitesh"
+console.log(Myname)
+console.log(anotherName)
+
+let firstOne={
+    name:"ram",
+    age:22,
+    
+}
+let second=firstOne
+second.name="rahul"
+console.log(firstOne.name)
+console.log(second.name)
+
