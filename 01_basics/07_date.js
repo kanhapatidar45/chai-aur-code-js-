de447d1224e8ae0e3c1f1359d0e77148e91579bb
Kanha patidar <124128569+kanhapatidar45@users.noreply.github.com>
2024-11-14 18:04:49 +0000

    date

---
+++ 01_basics/07_date.js
@@ -0,0 +1,39 @@
+//current date syntax
+// let myDate=new Date()
+// console.log(myDate.toDateString())
+// console.log(myDate.toISOString())
+// console.log(myDate.toJSON())
+// console.log(myDate.toLocaleDateString())
+// console.log(myDate.toLocaleString())
+// console.log(myDate.toLocaleTimeString())
+// console.log(myDate.toString())
+// console.log(myDate.toTimeString())
+// console.log(myDate.toUTCString())
+// console.log(myDate.getTimezoneOffset())
+
+//user defined date
+// let userDate=new Date(2024,0,6)
+// console.log(userDate.toDateString())
+// let userDate=new Date(2024 , 0 , 6 , 5 , 3)
+// let userDate=new Date( "2022-10-11")
+// let userDate=new Date( "11-10-2021")
+// console.log(userDate.toLocaleString())
+
+
+let myCreatedDate=new Date(2021,3,18)
+let myTimeStamp=Date.now()
+console.log(myTimeStamp)
+console.log(myCreatedDate.getTime())
+
+//time seconds m chaiye to
+// console.log(Math.floor(Date.now()/100))
+
+
+// let newDate=new Date()
+// console.log(newDate)
+// console.log(newDate.getFullYear())
+// console.log(newDate.toLocaleString())
+
+// console.log(newDate.toLocaleString('default',{
+//     weekday:"long"
+// }))
