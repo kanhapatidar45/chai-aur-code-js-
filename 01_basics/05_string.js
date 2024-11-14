f4d16bca7bdd9a7e32416d9cbc439d17e071e81a
Kanha patidar <124128569+kanhapatidar45@users.noreply.github.com>
2024-11-14 10:01:47 +0000

    string

---
+++ 01_basics/05_string.js
@@ -0,0 +1,54 @@
+// const name='kanha'
+// const repo=44
+// console.log(name+repo+"value")//ye old tarika h hum new use krege jo ye h
+
+
+// console.log(`${name} ${repo} value`)// ye shi h
+
+
+
+/**********string define krne ka tarika isse hum bhot sare method mil jaegi jo already terminal m h aur hum
+ unka use kr sakte h jaise lowercase , upercase, slice aur bhi bhot********** */
+
+/********string methods difff diff  */
+
+//specific element
+ const gamename=new String("pubg-bgmi")
+//  console.log(gamename[0])
+
+//  //length
+//  console.log(gamename.length)
+
+//  //lower or uppercase
+//  console.log(gamename.toUpperCase())
+
+//  //char at pos
+//  console.log(gamename.charAt(2))
+
+
+//  //pos at character
+//  console.log(gamename.indexOf('u'))
+
+//  //substring
+//  console.log(gamename.substring(0,2))
+
+
+//slice m ky hoga ki agar start>=arr.length an emoty array return and  0>start <=arr.length then start=start+arr.length same for end
+// console.log(gamename.slice(1,6))
+// console.log(gamename.slice(-6,5))
+
+//trim
+// const newName=new String(" hero ")
+// console.log(newName)
+// console.log(newName.trim())
+
+
+//replace
+// console.log(gamename.replace('-','#'))
+
+//includes
+// console.log(gamename.includes('bg'))
+
+//split
+// console.log(gamename.split('-'))
+
